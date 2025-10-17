4415f5e11226431903096c96637487646fd215e4
Natsu_Dragnir_ <94977973+NatsuDragneer@users.noreply.github.com>
2025-10-17 23:05:39 +0200

    change

--- core/logic.py
+++ core/logic.py
@@ -3,10 +3,3 @@ core/logic.py
 Contient la logique de l'application (calculs, traitements...).
 """
 
-def additionner(a: float, b: float) -> float:
-    """Additionne deux nombres."""
-    return a + b
-
-def message_bienvenue(nom: str) -> str:
-    """Retourne un message personnalisé."""
-    return f"Bienvenue, {nom} 🐉🔥"
